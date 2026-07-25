a55d51a2941bcf3dad0981c93e61cf45fd8b4db6
cathrinemary0208 <cathrinemary0208@gmail.com>
2026-07-25 22:19:52 +0530

    Add data.js and module.js

---
+++ nodejsCourse_repo/data.js
@@ -0,0 +1,14 @@
+//code io practise ---------
+
+let names = ["C Language","Python","C++","Java"]
+let age = [24,45,78,90]
+let year = [2024,2025,2026]
+console.log(names,age)
+
+module.exports = {
+    Name : names ,
+    Age  : age ,
+    Year : year
+}
+
+
---
+++ nodejsCourse_repo/module.js
@@ -0,0 +1,12 @@
+//code io practise -------------
+
+const data = require("./data")
+console.log(data.Name) /// to extract a single value
+console.log(data)  
+//To only import the required data 
+//available in the same name
+const {year} = require("./data")
+console.log(year)
+//default modules
+const os = require('os')
+console.log(os)
