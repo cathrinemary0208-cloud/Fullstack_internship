6b5661032f3b05a08ae8cf4863568ccacfe04306
cathrinemary0208 <cathrinemary0208@gmail.com>
2026-07-25 21:46:04 +0530

    Add modules folder,data.js,module.js

---
+++ module.js
@@ -0,0 +1,11 @@
+//code io practise -------------
+
+const data = require("./data")
+console.log(data.Name) /// to extract a single value
+console.log(data)  
+//To only import the required data 
+//available in the same name
+const {year} = require("./data")
+console.log(year)
+
+
