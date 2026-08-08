9a8357703ae2052900f355ef289c8b327d23a731
cathrinemary0208 <cathrinemary0208@gmail.com>
2026-08-08 15:49:24 +0530

    A

--- Server.js
+++ Server.js
@@ -1,17 +1,36 @@
 const http = require('http');
+const fs = require('fs')
+const readStream = fs.createReadStream('./index.html',{encoding:"utf8"})
+
 //we are creating a server 
+
 const server = http.createServer((request,response) => {
   console.log("Request made ...")
-  console.log(request,response)
-  console.log(request.url) //gives the url  ignoring the 3000
-  console.log(request.method)//To know which type of request it is 
 
-  //Sending a response we need to set response header 
+
+  // console.log(request,response)
+  // console.log(request.url) //gives the url  ignoring the 3000
+  // console.log(request.method)//To know which type of request it is 
+  //Sending a response we need to set response header
+
   response.setHeader('Content-type','text/html');
   //writing the response 
-  response.write('<h1>Hello,World ,this is my first server response</h1>');
+  
+  fs.readFile('./index.html',(err,data)=>
+  {
+    if(err)
+    {
+      console.log(err.message)
+    }
+    else
+    {
+      response.write(data);
+      response.end();
+    }
+  })
+  //response.write('<h1>Hello,World ,this is my first server response</h1>');
   //we should end the response if not we wont get the output in the browser
-  response.end();
+  //response.end();
   
 }
 )
--- nodejsCourse_repo/modules/greetings.js
+++
@@ -1,4 +0,0 @@
-const greet = (name) => `Hello ,${name} Greetings to you `
-//returns only a single function
-module.exports = greet;
-
