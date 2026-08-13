5b5729d75ed74867171a5a2171632fd5eda32a35
cathrinemary0208 <cathrinemary0208@gmail.com>
2026-08-13 09:06:30 +0530

    save changes

--- ExpressjsCourse/index.mjs
+++ ExpressjsCourse/index.mjs
@@ -1,11 +1,74 @@
+//working with arrays and route parameters
 import express from "express"
 
 const app = express();
 
 const PORT = 3000;
+//defin
+const users = [
+    {"id":1,"user_name":"Cathy"},
+    {"id":2,"user_name":"Rubena"},
+    {"id":3,"user_name":"Jayanthi"},
+    {"id":4,"user_name":"Lance"},
+    {"id":5,"user_name":"Srinithi"},
+    {"id":6,"user_name":"Nidiya"},
+]
+
+const products = [
+    {"id":1,"product_name":"Soap"},
+    {"id":2,"product_name":"Fan"},
+    {"id":3,"product_name":"Washing machine"},
+    {"id":4,"product_name":"Fridge"},
+    {"id":5,"product_name":"Electric stove"},
+    {"id":6,"product_name":"Mirror"},
+]
+app.get("/api/products",(req,res)=>
+{
+    res,send(products)
+})
+app.get("/api/users",(req,res)=>
+{
+    res.send(users)
+})
+app.get("/api/products/:id",(req,res)=>
+{
+    const id = parseInt(req.params.id);
+    if(isNaN(id))
+    {
+        res.status(400).send({"Message":"Not a Number .Enter valid url"})
+    }
+    const product = products.find((product)=> product.id === id)
+    if(product)
+    {
+        res.send(product)
+    }
+    res.status(404).send({"Message" :"Product not Found"})
+
+})
+//specifying the id in url
+app.get("/api/users/:id",(req,res)=>
+{
+   const id = parseInt(req.params.id);
+   //checking if the id is a string
+   if(isNaN(id))
+   {
+      res.status(400).send({"Message" :"This is not a number"})
+   }
+
+    //finding the id in the users array
+   const user = users.find((user)=>user.id === id)
+   //if user present send it or ...
+   if(user)
+   {
+     res.send(user)
+   }
+   res.status(404).send({"Message":"User not found "})
+})
 
 app.listen(PORT,()=>
 {
     console.log(`Server is listening in port  in  ${PORT}`)
 })
+
+//Query parameters
 
