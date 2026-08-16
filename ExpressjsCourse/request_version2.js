dea559489e77d054c34c5cc44f67f88aac007b76
cathrinemary0208 <cathrinemary0208@gmail.com>
2026-08-16 17:41:38 +0530

    Add express-validator for input validation and enhance item management API

--- ExpressjsCourse/request_version2.js
+++ ExpressjsCourse/request_version2.js
@@ -1,6 +1,10 @@
-//Sending data ffrom frontend to backend is callled post request
+//This files works on avoiding all the Redundant data
+//using custom middleware 
+//and also validation in express js - step1:install express validator( npm i express-validator)
 import express from "express"
-
+import { checkSchema,validationResult,matchedData } from "express-validator";
+//importing the file 
+import {createItemValidationSchema} from "./utils/validation_Schemas.js"
 const app = express()
 
 app.use(express.json()); //middleware to convert it as a json file
@@ -12,6 +16,28 @@ const items = [
   { id: 4, name: "Umbrella", category: "Accessories", price: 12.99 },
   { id: 5, name: "Table Fan", category: "Appliances", price: 22.5 }
 ];
+
+//Custom middleware 
+const getItemIndex = (req,res,next)=>{
+  
+    //getting the id from the parameter
+    const id = parseInt(req.params.id)
+     //checking if it is a number
+    if(isNaN(id))
+    {
+        return res.status(404).send({"Message":"Bad Request ,Invalid ID"})
+    }
+    //obtaning the index based on parameter's id
+    const itemIndex = items.findIndex((item)=>item.id===id)
+    //checking if the item present
+    if(items[itemIndex]==-1)
+    {
+        return res.status(404).send({"Message":"Item not found ,Try again"})
+    }
+    req.itemIndex = itemIndex;
+    req.id = id;
+    next();
+}
 
 
 app.get("/api/items",(req,res)=>
@@ -25,11 +51,8 @@ app.get("/api/items",(req,res)=>
     
 })
 
-app.get("/api/items/:id", (req, res) => {
-    const id = parseInt(req.params.id)
-    if (isNaN(id)) {
-        return res.status(404).send({ "Message": "Not a Number" })
-    }
+app.get("/api/items/:id",getItemIndex,(req, res) => {
+    const{id} = req;
     const item = items.find((item) => item.id === id)
     if (!item) {
         return res.status(404).send({ "Message": "Item not found" })
@@ -44,10 +67,17 @@ app.get("/api/items/:id", (req, res) => {
 // Attaches the result to req.body
 // Calls next() so your route handler can then use req.body
 
-app.post("/api/items_",(req,res)=>
+app.post("/api/items_",checkSchema(createItemValidationSchema),(req,res)=>
 {
+    //Checking the result of check schema
+    const result = validationResult(req);
+    console.log(req)
+    if(!result.isEmpty())
+    {
+        return res.status(400).send({error:result.array()})
+    }
+    const body = matchedData(req)
     console.log(req.body)
-    const {body} = req
     const newUser = {id:items[items.length-1].id+1,...body}
     items.push(newUser)
     console.log("Array length after push:", items.length)   
@@ -55,22 +85,11 @@ app.post("/api/items_",(req,res)=>
     return res.status(201).send(newUser)
 })
 //PUT request - to update completely 
-app.put("/api/items/:id",(req,res)=>
+app.put("/api/items/:id",getItemIndex,(req,res)=>
 {
-    //getting the id from the parameter
-    const id = parseInt(req.params.id)
-    //checking if it is a number
-    if(isNaN(id))
-    {
-        return res.send({"Message":"Bad Request ,Invalid ID!!!"})
-    }
-    //obtaning the index based on parameter's id
-    const itemIndex = items.findIndex((item)=>item.id===id)
-    //checking if the item present
-    if(items[itemIndex]===-1)
-    {
-       return  res.status(404).send({"Message":"Item not found!!!"})
-    }
+     const {itemIndex} = req;
+     const {id} = req;
+     console.log(itemIndex)
     //getting the body from request by destructuring  
      const {body} = req;
      //In the index we are updating the values
@@ -79,37 +98,18 @@ app.put("/api/items/:id",(req,res)=>
 })
 
 //PATCH request - To partially update the data 
-app.patch("/api/items/:id",(req,res)=>
+app.patch("/api/items/:id",getItemIndex,(req,res)=>
 {
-    const id = parseInt(req.params.id)
-    if(isNaN(id))
-    {
-        return res.status(404).send({"Message":"Bad Request ,Invalid ID"})
-    }
-    const itemIndex = items.findIndex((item)=>item.id === id)
-    if(items[itemIndex]=== -1)
-    {
-         return res.status(404).send({"Message":"Bad Request ,Item not found"})
-    }
+    const {itemIndex} = req;
     const {body} = req;
     //Already present data + The new data (body)
     items[itemIndex] = {...items[itemIndex],...body}
     res.send("Updated Successfully")
 })
 
-app.delete("/api/items/:id",(req,res)=>
+app.delete("/api/items/:id",getItemIndex,(req,res)=>
 {
-    const id = parseInt(req.params.id)
-    if(isNaN(id))
-    {
-        return res.status(404).send({"Message":"Invalid ID ..."})
-    }
-    const itemIndex = items.findIndex((item)=>item.id === id)
-    if(items[itemIndex]===-1)
-    {
-      return res.status(404).send({"Message":"Item not found"})  
-    }
-    //
+    const {itemIndex} = req;
     items.splice(itemIndex,1)
     return res.send({"Message":"Deleted successfully"})
 
---
+++ ExpressjsCourse/utils/validation_Schemas.js
@@ -0,0 +1,47 @@
+//For the request body we are writing some validations here
+// Request comes in
+//       ↓
+// checkSchema(schema) middleware runs
+//   → validates req.body.name  → stores result on req
+//   → validates req.body.price → stores result on req
+//   → validates req.body.category → stores result on req
+//       ↓
+//  route handler runs
+//   → validationResult(req)  → reads stored results → "any errors?"
+//   → matchedData(req)       → reads stored results → "give me the valid {name, price, category}"
+
+import { checkSchema } from "express-validator";
+export const createItemValidationSchema = {
+
+    name : {
+        notEmpty:{
+            errorMessage : "Item Name should not be empty"
+        },
+        isLength:{
+            options:{min:3,max:10},
+            errorMessage:"Length should be between 3 and 10"
+        }
+    },
+    category:
+    {
+        notEmpty:{
+            errorMessage : "Item category should not be empty"
+        },
+        isLength:{
+            options:{min:3,max:10},
+            errorMessage:"Length should be between 3 and 10"
+        } ,  
+        isString:{
+            options:true,
+            errorMessage :"Should be a string"
+        }
+
+    },
+    price:
+    {
+        notEmpty:{
+            errorMessage : "price should not be empty"
+        }
+
+    }
+}
